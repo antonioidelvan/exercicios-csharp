518f02d7ea3d95bc3d2fc8d33cbc6d9b424f2b3b
Antonio Sousa <89809020+antonioidelvan@users.noreply.github.com>
2021-09-02 22:01:25 -0400

    Exercício 1
    Faça  um  programa  em  C#  que  leia  o  valor  de  uma  mercadoria  e  a porcentagem  de  desconto.  O  algoritmo  deverá  calcular  e  mostrar  o  valor  da mercadoria a ser paga considerando o desconto concedido.

---
+++ exerc1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+// Faça  um  programa  em  C#  que  leia  o  valor  de  uma  mercadoria  e  a porcentagem  de  desconto.  O  algoritmo  deverá  calcular  e  mostrar  o  valor  da mercadoria a ser paga considerando o desconto concedido.
+
+namespace exercicios_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Valor da mercadoria
+            float mercadoriaValor;
+            // Porcentagem de desconto
+            int porcentagemDesconto;
+            // Valor final com desconto
+            float valorDesconto;
+            // Valor fixo para porcentagem
+            int porcentagem = 100;
+            // Valor final
+            float valorFinal;
+
+            Console.WriteLine("Qual o valor da mercadoria?");
+            mercadoriaValor = float.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a % de desconto?");
+            porcentagemDesconto = int.Parse(Console.ReadLine());
+
+            valorDesconto = mercadoriaValor * porcentagemDesconto/porcentagem;
+            valorFinal = mercadoriaValor - valorDesconto;
+
+            Console.WriteLine("Você ira pagar: R$ " + valorFinal);
+        }
+    }
+}
