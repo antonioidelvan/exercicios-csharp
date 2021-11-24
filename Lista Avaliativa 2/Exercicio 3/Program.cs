09cd0822c31628815aaee7759c8d9e817488f839
Antonio Sousa <89809020+antonioidelvan@users.noreply.github.com>
2021-11-24 16:10:43 -0400

    Adição Exercício 3

--- Lista Avaliativa 2/Exercicio 3/Program.cs
+++ Lista Avaliativa 2/Exercicio 3/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int tamanho = 5;
+
+int[] primeiroVetor = new int[tamanho];
+int[] segundoVetor = new int[tamanho];
+int[] juncaoVetor = new int[10];
+
+int primeiroIndex = 0, segundoIndex = 0;
+
+for (int i = 0; i < tamanho; i++)
+{
+    Console.Write("Digite um valor inteiro para o Primeiro vetor: ");
+    primeiroVetor[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < tamanho; i++)
+{
+    Console.Write("Digite um valor inteiro para o Segundo vetor: ");
+    segundoVetor[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < 10; i++)
+{
+    if (i % 2 == 0)
+    {
+        juncaoVetor[i] = primeiroVetor[primeiroIndex];
+        primeiroIndex++;
+    }
+    else
+    {
+        juncaoVetor[i] = segundoVetor[segundoIndex];
+        segundoIndex++;
+    }
+
+}
+
+foreach (int valores in juncaoVetor)
+{
+    Console.WriteLine(valores);
+}
