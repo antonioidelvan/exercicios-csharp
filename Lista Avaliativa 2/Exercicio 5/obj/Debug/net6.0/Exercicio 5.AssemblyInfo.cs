fb9206c52c9536aa0a0a24b5e0a6265f6691db10
Antonio Sousa <89809020+antonioidelvan@users.noreply.github.com>
2021-11-24 14:40:26 -0400

    Update

--- Lista Avaliativa 2/Exercicio 5/obj/Debug/net6.0/Exercicio 5.AssemblyInfo.cs
+++ Lista Avaliativa 2/Exercicio 5/obj/Debug/net6.0/Exercicio 5.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("exercicio 9")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio 5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("exercicio 9")]
-[assembly: System.Reflection.AssemblyTitleAttribute("exercicio 9")]
+[assembly: System.Reflection.AssemblyProductAttribute("Exercicio 5")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Exercicio 5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
